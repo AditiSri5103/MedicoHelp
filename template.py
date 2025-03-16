9baf425543dd7918aaad1889b89dd0fb4c1ebfbc
Aditi Srivastava <aditisrivastava7788@gmail.com>
2025-03-17 00:53:04 +0530

    Added folder structure

--- template.py
+++ template.py
@@ -19,4 +19,12 @@ for filepath in list_of_files:
     
     if filedir!="":
         os.makedirs(filedir,exist_ok=True)
-        logging.info("")
+        logging.info(f"Creating directory: {filedir} for the file: {filename}")
+
+    if (not os.path.exists(filepath) or (os.path.getsize(filepath) == 0)):
+        with open(filepath,"w") as f:
+            pass 
+            logging.info(f"Creating empty file: {filepath}")
+
+    else:
+        logging.info(f"{filename} already exists")
